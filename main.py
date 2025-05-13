192d8f5fcb61c77b5f3379aee651f91a8c185a8a
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-13 10:56:14 +0100

    add input options

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas
 
+morse_running = True
+
 morse_code_df = pandas.read_csv("morse_code.csv")
 
 letter_to_morse_code_dict = {row.char: row.code for (index, row) in morse_code_df.iterrows()}
@@ -30,8 +32,24 @@ def convert_morse_to_text(morse_input):
 
 
 
-user_input = input("Enter the text you wish to convert to Morse code: ").upper().strip()
-print(f"{user_input.title()} is {convert_text_to_morse(user_input)} in Morse Code.")
+if __name__ == "__main__":
+    while morse_running:
+        user_choice = input("Do you wish to (1) Convert Text to Morse, (2) Convert Morse to Text, (3) Quit: ")
+        if user_choice.strip() == "1":
+            user_input = input("Enter the text you wish to convert to Morse code: ").upper().strip()
+            print(f"{user_input.title()} is {convert_text_to_morse(user_input)} in Morse Code.")
+        elif user_choice.strip() == "2":
+            morse_user_input = input("Enter the Morse Code you wish to convert to text: ").upper().strip()
+            print(f'{morse_user_input} is "{convert_morse_to_text(morse_user_input).lower()}" in normal text.')
+        elif user_choice.strip() == "3":
+            print("Goodbye!")
+            morse_running = False
+        else:
+            print("Invalid input. Please try again.")
 
-morse_user_input = input("Enter the Morse Code you wish to convert to text: ").upper().strip()
-print(f"{morse_user_input} is {convert_morse_to_text(morse_user_input)} in normal text.")
+
+
+
+
+
+
